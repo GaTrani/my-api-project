c9f1ebf2383fd2c81ca46183c60fddcfa9be4d4b
GaTrani <gabrieltrani02@gmail.com>
2024-11-28 15:15:46 -0300

    Upload base project.

---
+++ src/main/java/com/example/my_api_project/MyApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiProjectApplication.class, args);
+	}
+
+}
